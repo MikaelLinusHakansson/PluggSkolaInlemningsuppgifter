8886a3951a07e4b9842d85a663b77b4b6d22e5cc
Linus Håkansson <Linus.Hakansson@yh.nackademin.se>
2022-10-01 10:13:09 +0200

    Initial commit - laddar upp inlämningsuppfit 1

---
+++ src/Sprint1/Inlämningsuppgift1/InlämingMain.java
@@ -0,0 +1,11 @@
+package Sprint1.Inlämningsuppgift1;
+public class InlämingMain {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
+
+
+
+
+// https://studentportal.nackademin.se/pluginfile.php/298419/mod_resource/content/0/Inl%C3%A4mningsuppgift%201_uppdaterad_DISTANS.pdf
